718b0abbc953d51307d504d42b274cb8e679cb6a
Blake Loring <blake_l@parsed.uk>
2015-07-14 13:37:27 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ extern {
   fn klee_int(name: *const raw::c_char) -> raw::c_int;
   fn klee_warning(name: *const raw::c_char);
   fn klee_warning_one(name: *const raw::c_char);
+  fn klee_set_forking(state: raw::c_bool);
 }
 
 pub unsafe fn any(data: *const raw::c_void, length: usize, name: &str) {
@@ -24,6 +25,10 @@ pub fn warning_one(name: &str) {
   unsafe { klee_warning_once(CString::new(name).unwrap().as_ptr()); }
 }
 
+pub fn set_forking(state: bool) {
+  unsafe { klee_set_forking(state as raw::c_bool); }
+}
+
 pub fn i32(name: &str) -> i32 {
   let result;
   unsafe {
@@ -32,6 +37,6 @@ pub fn i32(name: &str) -> i32 {
   result
 }
 
-pub fn sym<T>(data: &T, name: &str) {
+pub fn symbol<T>(data: &T, name: &str) {
   unsafe{ any(transmute(data as *const T), size_of::<T>(), name); }
 }
