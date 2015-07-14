9f6bc0e10fbb3c036ed871158f078c02b8529b3f
Blake Loring <blake_l@parsed.uk>
2015-07-14 13:31:24 +0100

    Update simple.rs

--- tests/simple.rs
+++ tests/simple.rs
@@ -2,6 +2,23 @@ use klee;
 
 #[test]
 fn basic_test() {
-  let a = 53;
-ads g klee::sym(&a, "a");
+  let mut a : i32;
+  klee::sym(&a, "a");
+  assert_eq!(a, 56);
+}
+
+#[test]
+fn other_test() {
+  let mut a : i32;
+  let mut b : i32;
+  
+  klee::sym(&a, "a");
+  klee::sym(&b, "b");
+  
+  if a == 50 {
+    if b == 49 {
+      //Error;
+      println!("Errroooor");
+    }
+  }
 }
