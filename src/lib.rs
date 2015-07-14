b2698899e41730babf0d2f2909a4cd3120904ca4
Blake Loring <blake_l@parsed.uk>
2015-07-14 22:31:30 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,8 @@ extern {
     fn klee_set_forking(state: bool);
 }
 
-#[link(name = "klee-libc")]
-extern {
-    fn klee_warning_once(name: *const raw::c_char);
-}
-
 pub unsafe fn any(data: *const raw::c_void, length: usize, name: &str) {
     klee_make_symbolic(data, length as libc::size_t, CString::new(name).unwrap().as_ptr());
-}
-
-pub fn warning_once(name: &str) {
-    unsafe { klee_warning_once(CString::new(name).unwrap().as_ptr()); }
 }
 
 pub fn set_forking(state: bool) {
@@ -41,10 +32,4 @@ pub fn bool(name: &str) ->bool {
 
 pub fn symbol<T>(data: &T, name: &str) {
     unsafe{ any(transmute(data as *const T), size_of::<T>(), name); }
-}
-
-pub fn assert_warn(condition: bool, msg: &str) {
-    if condition {
-        warning_once(msg);
-    }
 }
