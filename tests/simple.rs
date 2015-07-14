35516b4ee8e7e66d6da4cb3fd9e7e3dae9d4c682
Blake Loring <blake_l@parsed.uk>
2015-07-14 16:28:36 +0100

    Update simple.rs

--- tests/simple.rs
+++ tests/simple.rs
@@ -24,16 +24,15 @@ fn other_test() {
 
 #[test]
 fn another_test() {
-  let mut a : i32;
-  klee::symbol(&a, "a");
+  let a = klee::i32();
   if a > 60 && a < 90 {
     let b = a + 40;
     klee::assert_warn(b == 0, "this assertion should not trigger");
-    
+
     if b == 12 {
       klee::warning("This path should be unreachable");
     }
-    
+
     klee::assert_warn(b == 101, "this assertion should trigger");
     klee::assert_warn(b == 150000, "this assertsion should not trigger");
   }
