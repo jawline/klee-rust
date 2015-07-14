6a0b042ff8a34167cbade59747a42de7fcc9fc66
Blake Loring <blake_l@parsed.uk>
2015-07-14 09:59:08 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 extern crate libc;
 
+use std::ffi::CString;
+
 #[link(name = "rust")]
 extern {
   fn klee_make_symbolic(data: *const libc::c_void, length: usize, name: *const libc::c_char);
+}
+
+pub fn klee_make_symbolic_any(data: *const libc::c_void, length: usize, name: &str) {
+  let name_cstr = CString::new(name).unwrap();
+  unsafe {
+    klee_make_symbolic(data, length, name_cstr.as_ptr());
+  }
 }
