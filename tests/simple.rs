890ffaaf12476d19c37a6044486399a4d0efbdf9
Blake Loring <blake_l@parsed.uk>
2015-07-14 16:26:42 +0100

    Update simple.rs

--- tests/simple.rs
+++ tests/simple.rs
@@ -3,7 +3,7 @@ use klee;
 #[test]
 fn basic_test() {
   let mut a : i32;
-  klee::sym(&a, "a");
+  klee::symbol(&a, "a");
   assert_eq!(a, 56);
 }
 
@@ -12,13 +12,29 @@ fn other_test() {
   let mut a : i32;
   let mut b : i32;
   
-  klee::sym(&a, "a");
-  klee::sym(&b, "b");
+  klee::symbol(&a, "a");
+  klee::symbol(&b, "b");
   
   if a == 50 {
     if b == 49 {
-      //Error;
-      println!("Errroooor");
+      klee::warning("this path is reachable");
     }
+  }
+}
+
+#[test]
+fn another_test() {
+  let mut a : i32;
+  klee::symbol(&a, "a");
+  if a > 60 && a < 90 {
+    let b = a + 40;
+    klee::assert_warn(b == 0, "this assertion should not trigger");
+    
+    if b == 12 {
+      klee::warning("This path should be unreachable");
+    }
+    
+    klee::assert_warn(b == 101, "this assertion should trigger");
+    klee::assert_warn(b == 150000, "this assertsion should not trigger");
   }
 }
